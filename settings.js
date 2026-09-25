c4b5fd401596ea58f3ef0373d6deac469f178b95
sanjuislam200567-commits <sanjuislam200567@gmail.com>
2026-09-25 17:31:21 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'CEO RATUL_BOT',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "CEO RATUL BOT",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+8801610386604', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
